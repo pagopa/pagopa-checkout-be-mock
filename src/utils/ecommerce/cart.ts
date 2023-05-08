55854a6ec1562a319776cd1c8094f0599e5e96f0
Pietro Tota <pietro.tota@pagopa.it>
2023-05-08 19:58:23 +0200

    fix: change CARTS response adding multiple payment notices

--- src/utils/ecommerce/cart.ts
+++ src/utils/ecommerce/cart.ts
@@ -7,15 +7,43 @@ export const createSuccessGetCartResponseEntity = (): CartRequest => ({
   paymentNotices: [
     {
       amount: 1000,
-      companyName: "test",
-      description: "test",
+      companyName: "test1",
+      description: "test1",
       fiscalCode: "77777777777",
-      noticeNumber: "302012387654312384"
+      noticeNumber: "302012387654312300"
+    },
+    {
+      amount: 2000,
+      companyName: "test2",
+      description: "test2",
+      fiscalCode: "77777777777",
+      noticeNumber: "302012387654312301"
+    },
+    {
+      amount: 3000,
+      companyName: "test3",
+      description: "test3",
+      fiscalCode: "77777777777",
+      noticeNumber: "302012387654312302"
+    },
+    {
+      amount: 4000,
+      companyName: "test4",
+      description: "test4",
+      fiscalCode: "77777777777",
+      noticeNumber: "302012387654312303"
+    },
+    {
+      amount: 5000,
+      companyName: "test5",
+      description: "test5",
+      fiscalCode: "77777777777",
+      noticeNumber: "302012387654312304"
     }
   ],
   returnUrls: {
-    returnCancelUrl: "www.comune.di.prova.it/pagopa/cancel.html",
-    returnErrorUrl: "www.comune.di.prova.it/pagopa/error.html",
-    returnOkUrl: "www.comune.di.prova.it/pagopa/success.html"
+    returnCancelUrl: "https://www.comune.di.prova.it/pagopa/cancel.html",
+    returnErrorUrl: "https://www.comune.di.prova.it/pagopa/error.html",
+    returnOkUrl: "https://www.comune.di.prova.it/pagopa/success.html"
   }
 });
