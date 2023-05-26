c5aef4b1ff393861085c15ae370216c53c203a6a
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-05-26 02:01:29 +0200

    add cookie policy to navigate final page

--- src/handlers/ecommerce/transaction.ts
+++ src/handlers/ecommerce/transaction.ts
@@ -1,18 +1,179 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { RequestHandler } from "express";
-import { FlowCase, getFlowCookie } from "../../flow";
+import {
+  FlowCase,
+  getFlowCookie,
+  getPaymentGatewayCookie,
+  getSendPaymentResultCookie
+} from "../../flow";
 import { logger } from "../../logger";
 import {
   createSuccessGetTransactionEntity,
   error404TransactionIdNotFound,
   internalServerError500
 } from "../../utils/ecommerce/transaction";
+import { TransactionStatusEnum } from "../../generated/ecommerce/TransactionStatus";
 
 export const ecommerceGetTransaction: RequestHandler = async (req, res) => {
   logger.info("[Get transaction ecommerce] - Return success case");
-  res
-    .status(200)
-    .send(createSuccessGetTransactionEntity(req.params.transactionId));
+  const gateway = getPaymentGatewayCookie(req);
+  const errorCode = undefined;
+  const sendPaymentResultOutcome = getSendPaymentResultCookie(req);
+  switch (getFlowCookie(req)) {
+    case FlowCase.NOTIFICATION_REQUESTED:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.NOTIFICATION_REQUESTED,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.NOTIFICATION_ERROR:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.NOTIFICATION_ERROR,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.NOTIFIED_KO:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.NOTIFIED_KO,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.REFUNDED:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.REFUNDED,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.REFUND_REQUESTED:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.REFUND_REQUESTED,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.REFUND_ERROR:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.REFUND_ERROR,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.CLOSURE_ERROR:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.CLOSURE_ERROR,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.EXPIRED_NOT_AUTHORIZED:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.EXPIRED_NOT_AUTHORIZED,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.CANCELED:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.CANCELED,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.CANCELLATION_EXPIRED:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.CANCELLATION_EXPIRED,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.EXPIRED:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.EXPIRED,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    case FlowCase.UNAUTHORIZED:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.UNAUTHORIZED,
+            gateway,
+            errorCode,
+            sendPaymentResultOutcome
+          )
+        );
+    default:
+      return res
+        .status(200)
+        .send(
+          createSuccessGetTransactionEntity(
+            req.params.transactionId,
+            TransactionStatusEnum.NOTIFIED_OK
+          )
+        );
+  }
 };
 
 const ecommerceDeleteTransaction500 = (res: any): void => {
