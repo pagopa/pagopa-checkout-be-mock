03fcfa8ce43f3b7743a4edb307aa502ce45a36ad
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-08-22 17:40:02 +0200

    update payment methods api

--- src/handlers/ecommerce/payment-method.ts
+++ src/handlers/ecommerce/payment-method.ts
@@ -1,84 +1,70 @@
 /* eslint-disable sort-keys */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as https from "https";
 import { RequestHandler } from "express";
-import nodeFetch from "node-fetch";
-import { pipe } from "fp-ts/lib/function";
 import { logger } from "../../logger";
 import { createSuccessGetPaymentMethods } from "../../utils/ecommerce/payment-method";
-import { createClient as createNpgClient } from "../../generated/ecommerce_npg_client/client";
 
 export const ecommerceGetPaymentMethods: RequestHandler = async (req, res) => {
   logger.info("[Get payment-methods ecommerce] - Return success case");
   res.status(200).send(createSuccessGetPaymentMethods());
 };
 
-export const npgClient = createNpgClient({
-  baseUrl:
-    "https://stg-ta.nexigroup.com/api/phoenix-0.0/psp/api/v1/api/orders/build",
-  fetchApi: (nodeFetch as any) as typeof fetch
-});
+const { X_API_KEY } = process.env;
 
-export const createFormWithNpg: RequestHandler = async (req, res) => {
-  pipe(() =>
-    npgClient.apiordersbuild({
-      ApiKeyAuth: "",
-      "Correlation-Id": "",
-      body: {
-        merchantUrl: "https://localhost:1234",
-        order: {
-          amount: "0",
-          currency: "EUR",
-          orderId: "btid23838555"
-        },
-        paymentSession: {
-          actionType: "VERIFY",
-          amount: "0",
-          cancelUrl: "https://checkout.pagopa/cancel",
-          language: "ITA",
-          notificationUrl: "https://merchanturl.it",
-          paymentService: "CARDS",
-          resultUrl: "https://checkout.pagopa.it/esito"
-        },
-        version: "2"
-      }
-    })
+export const createFormWithNpg: RequestHandler = async (_req, res) => {
+  logger.info(
+    `[Get Payment Method preauthorization id: ${_req.params.id}] - Return success case`
   );
-};
-
-/* export const createFormWithNpg: RequestHandler = async (req, res) => {
-  const body = {
-    body: {
-      merchantUrl: "https://localhost:1234",
-      order: {
-        amount: "0",
-        currency: "EUR",
-        orderId: "btid23838555"
-      },
-      paymentSession: {
-        actionType: "VERIFY",
-        amount: "0",
-        cancelUrl: "https://checkout.pagopa/cancel",
-        language: "ITA",
-        notificationUrl: "https://merchanturl.it",
-        paymentService: "CARDS",
-        resultUrl: "https://checkout.pagopa.it/esito"
-      },
-      version: "2"
-    }
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      "Correlation-Id": "df615d11-1159-43c9-bd07-fdc6f460e449",
+      "X-Api-key": X_API_KEY
+    },
+    hostname: "stg-ta.nexigroup.com",
+    maxRedirects: 20,
+    method: "POST",
+    path: "/api/phoenix-0.0/psp/api/v1/orders/build"
   };
 
-  logger.info("[Create form payment-methods ecommerce] - Return success case");
-  pipe(
-    () =>
-      fetch(
-        "https://stg-ta.nexigroup.com/api/phoenix-0.0/psp/api/v1/api/orders/build",
-        {
-          method: "POST",
-          body: JSON.stringify(body),
-          headers: { "Content-Type": "application/json" }
-        }
-      ),
-    O.map(result => result.json()),
-    O.map(jsonformat => res.status(200).send(jsonformat))
-  );
-}; */
+  const build = https.request(options, response => {
+    // eslint-disable-next-line functional/prefer-readonly-type
+    const chunks: Uint8Array[] = [];
+
+    response.on("data", chunk => {
+      // eslint-disable-next-line functional/immutable-data
+      chunks.push(chunk);
+    });
+
+    response.on("end", () => {
+      const body = Buffer.concat(chunks);
+      res.status(200).send(body);
+    });
+
+    response.on("error", error => {
+      logger.error(error);
+    });
+  });
+  const postData = JSON.stringify({
+    merchantUrl: `${_req.protocol}://${_req.get("Host")}`,
+    order: {
+      amount: "1000",
+      currency: "EUR",
+      orderId: "btid23838555"
+    },
+    paymentSession: {
+      actionType: "PAY",
+      amount: "1000",
+      cancelUrl: "https://checkout.pagopa/cancel",
+      language: "ITA",
+      notificationUrl: "https://merchanturl.it",
+      paymentService: "CARDS",
+      resultUrl: "https://checkout.pagopa.it/esito"
+    },
+    version: "2"
+  });
+
+  build.write(postData);
+
+  build.end();
+};
