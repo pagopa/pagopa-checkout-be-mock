529cff4822d67e52e522d3483954cb0dfd761422
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-08-23 14:56:30 +0200

    implement fetch solutions

--- src/handlers/ecommerce/payment-method.ts
+++ src/handlers/ecommerce/payment-method.ts
@@ -1,6 +1,6 @@
 /* eslint-disable sort-keys */
-import * as https from "https";
 import { RequestHandler } from "express";
+import fetch from "node-fetch";
 import { logger } from "../../logger";
 import { createSuccessGetPaymentMethods } from "../../utils/ecommerce/payment-method";
 
@@ -13,38 +13,9 @@ const { X_API_KEY } = process.env;
 
 export const createFormWithNpg: RequestHandler = async (_req, res) => {
   logger.info(
-    `[Get Payment Method preauthorization id: ${_req.params.id}] - Return success case`
+    `[Get Payment Method npg-session id: ${_req.params.id}] - Return success case`
   );
-  const options = {
-    headers: {
-      "Content-Type": "application/json",
-      "Correlation-Id": "df615d11-1159-43c9-bd07-fdc6f460e449",
-      "X-Api-key": X_API_KEY
-    },
-    hostname: "stg-ta.nexigroup.com",
-    maxRedirects: 20,
-    method: "POST",
-    path: "/api/phoenix-0.0/psp/api/v1/orders/build"
-  };
 
-  const build = https.request(options, response => {
-    // eslint-disable-next-line functional/prefer-readonly-type
-    const chunks: Uint8Array[] = [];
-
-    response.on("data", chunk => {
-      // eslint-disable-next-line functional/immutable-data
-      chunks.push(chunk);
-    });
-
-    response.on("end", () => {
-      const body = Buffer.concat(chunks);
-      res.status(200).send(body);
-    });
-
-    response.on("error", error => {
-      logger.error(error);
-    });
-  });
   const postData = JSON.stringify({
     merchantUrl: `${_req.protocol}://${_req.get("Host")}`,
     order: {
@@ -64,7 +35,22 @@ export const createFormWithNpg: RequestHandler = async (_req, res) => {
     version: "2"
   });
 
-  build.write(postData);
+  const response = await fetch(
+    "https://stg-ta.nexigroup.com/api/phoenix-0.0/psp/api/v1/orders/build",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Correlation-Id": "df615d11-1159-43c9-bd07-fdc6f460e449",
+        "X-Api-key": X_API_KEY as string
+      },
+      body: postData
+    }
+  );
 
-  build.end();
+  const jsonResponse = await response.json();
+  logger.info(
+    `[Get Payment Method npg-session status code: ${response.status}]`
+  );
+  res.status(response.status).send(jsonResponse);
 };
