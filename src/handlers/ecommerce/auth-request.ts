d5417d7892f3ad4b2c41eff56691c6d1baf53062
Fernando-Granato <117268775+Fernando-Granato@users.noreply.github.com>
2023-09-12 15:15:48 +0200

    feat(auth-request):added logic for invoke confirm payment NPG method (#59)
    * feat(auth-request):added logic for invoke confirm payment NPG method
    
    * feat(log): modified log into create form method
    
    * fix(auth-request):fixed problem auth request
    
    * feat(config):used config for new wnv variable
    
    * fix(response code):fix 409 response code
    
    * fix(fold):removed fold

--- src/handlers/ecommerce/auth-request.ts
+++ src/handlers/ecommerce/auth-request.ts
@@ -1,16 +1,112 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { RequestHandler } from "express";
-import { logger } from "../../logger";
+import * as TE from "fp-ts/lib/TaskEither";
+import { v4 as uuid } from "uuid";
+import * as O from "fp-ts/Option";
+import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
+import { FlowCase, getFlowCookie } from "../../flow";
 import {
   createSuccessAuthRequestResponseEntity,
+  createSuccessAuthRequestResponseEntityFromNPG,
+  error400InvalidRequestBody,
   error404TransactionIdNotFound,
   error409TransactionAlreadyProcessed
 } from "../../utils/ecommerce/auth-request";
-import { FlowCase, getFlowCookie } from "../../flow";
+import { logger } from "../../logger";
+import { RequestAuthorizationRequest } from "../../generated/ecommerce/RequestAuthorizationRequest";
+import {
+  PaymentInstrumentDetail2,
+  PaymentInstrumentDetail3
+} from "../../generated/ecommerce/PaymentInstrumentDetail";
+import { RequestAuthorizationResponse } from "../../generated/ecommerce/RequestAuthorizationResponse";
+import { config } from "../../config";
 
-const returnSuccessResponse = (res: any): void => {
-  logger.info("[Auth-request ecommerce] - Return success case");
-  res.status(200).send(createSuccessAuthRequestResponseEntity());
+const PSP_API_KEY = config.PSP_API_KEY;
+
+const confirmPaymentFromNpg = async (
+  _req: any,
+  res: any,
+  requestSessionId: string
+): Promise<void> => {
+  const postData = JSON.stringify({
+    amount: _req.body.amount,
+    sessionId: requestSessionId
+  });
+
+  logger.info(
+    `[Invoke NPG confirm payment with npg-session id: ${requestSessionId}] - Return success case`
+  );
+  const correlationId = uuid();
+  const url = `https://stg-ta.nexigroup.com/api/phoenix-0.0/psp/api/v1/build/confirm_payment`;
+  const response = await fetch(url, {
+    body: postData,
+    headers: {
+      "Content-Type": "application/json",
+      "Correlation-Id": correlationId,
+      "X-Api-key": PSP_API_KEY
+    },
+    method: "POST"
+  });
+
+  await pipe(
+    TE.tryCatch(
+      async () => response.json(),
+      _e => {
+        logger.error("Error invoking npg order build");
+      }
+    ),
+    TE.map(resp => {
+      pipe(
+        resp,
+        createSuccessAuthRequestResponseEntityFromNPG,
+        RequestAuthorizationResponse.decode,
+        E.fold(
+          () => {
+            logger.error("Error while invoke NPG unexpected body");
+            res.status(response.status).send(resp);
+          },
+          responseBody => res.status(response.status).send(responseBody)
+        )
+      );
+    }),
+    TE.mapLeft(() => res.status(response.status).send())
+  )();
+};
+
+const processAuthorizationRequest = (req: any, res: any): void => {
+  pipe(
+    req.body,
+    RequestAuthorizationRequest.decode,
+    O.fromEither,
+    O.fold(
+      () => res.status(400).send(error400InvalidRequestBody()),
+      resp => {
+        pipe(
+          resp.details,
+          PaymentInstrumentDetail3.decode,
+          O.fromEither,
+          O.fold(
+            () => {
+              pipe(
+                resp.details,
+                PaymentInstrumentDetail2.decode,
+                O.fromEither,
+                O.map(
+                  () =>
+                    res
+                      .status(200)
+                      .send(createSuccessAuthRequestResponseEntity()) // Type card invoke PGS
+                )
+              );
+            },
+            cardsDetails =>
+              confirmPaymentFromNpg(req, res, cardsDetails.sessionId) // Type card invoke NPG
+          )
+        );
+      }
+    )
+  );
 };
 
 const return409ErrorTransactionAlreadyProcessed = (
@@ -41,6 +137,6 @@ export const ecommerceAuthRequest: RequestHandler = async (req, res) => {
       return404ErrorTransactionIdNotFound(transactionId, res);
       break;
     default:
-      returnSuccessResponse(res);
+      processAuthorizationRequest(req, res);
   }
 };
--- src/utils/ecommerce/auth-request.ts
+++ src/utils/ecommerce/auth-request.ts
@@ -7,6 +7,14 @@ export const createSuccessAuthRequestResponseEntity = (): RequestAuthorizationRe
   authorizationUrl: "https://example.com"
 });
 
+export const createSuccessAuthRequestResponseEntityFromNPG = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  jsonResponse: any
+): RequestAuthorizationResponse => ({
+  authorizationRequestId: "requestId",
+  authorizationUrl: jsonResponse.url
+});
+
 export const error404TransactionIdNotFound = (
   transactionId: string
 ): ProblemJson => ({
@@ -26,4 +34,11 @@ export const error409TransactionAlreadyProcessed = (
   instance: "Example instance",
   status: 409 as HttpStatusCode,
   title: "Transaction already processed"
+});
+
+export const error400InvalidRequestBody = (): ProblemJson => ({
+  detail: "Invalid request body",
+  instance: "Instance",
+  status: 400 as HttpStatusCode,
+  title: "The request body is invalid"
 });
