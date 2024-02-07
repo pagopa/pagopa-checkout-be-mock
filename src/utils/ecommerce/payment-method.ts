3a7af98996d3c7ff9a10a749023bc089af2b7274
Fernando-Granato <fernando.granato@pagopa.it>
2024-02-07 11:52:14 +0100

    fix: method management

--- src/utils/ecommerce/payment-method.ts
+++ src/utils/ecommerce/payment-method.ts
@@ -1,11 +1,13 @@
 import { NonNegativeInteger } from "@pagopa/ts-commons/lib/numbers";
 import { PaymentMethodsResponse } from "../../generated/ecommerce/PaymentMethodsResponse";
 import { PaymentMethodStatusEnum } from "../../generated/ecommerce/PaymentMethodStatus";
+import { PaymentMethodManagementTypeEnum } from "../../generated/ecommerce/PaymentMethodManagementType";
 export const createSuccessGetPaymentMethods = (): PaymentMethodsResponse => ({
   paymentMethods: [
     {
       description: "Carte",
       id: "3ebea7a1-2e77-4a1b-ac1b-3aca0d67f813",
+      methodManagement: PaymentMethodManagementTypeEnum.ONBOARDABLE,
       name: "Carte",
       paymentTypeCode: "CP",
       ranges: [
@@ -19,6 +21,7 @@ export const createSuccessGetPaymentMethods = (): PaymentMethodsResponse => ({
     {
       description: "PostePay",
       id: "1c23629f-8133-42f3-ad96-7e6527d27a43",
+      methodManagement: PaymentMethodManagementTypeEnum.ONBOARDABLE,
       name: "PostePay",
       paymentTypeCode: "PPAY",
       ranges: [
