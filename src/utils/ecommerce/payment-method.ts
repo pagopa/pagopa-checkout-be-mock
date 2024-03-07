014e7a303c741ab8400a293e657528881f1e0659
Vito Falco <vitofalco@gmail.com>
2024-03-07 14:31:12 +0100

    Add a new paymantmethods list

--- src/utils/ecommerce/payment-method.ts
+++ src/utils/ecommerce/payment-method.ts
@@ -19,11 +19,69 @@ export const createSuccessGetPaymentMethods = (): PaymentMethodsResponse => ({
       status: PaymentMethodStatusEnum.ENABLED
     },
     {
-      description: "PostePay",
-      id: "1c23629f-8133-42f3-ad96-7e6527d27a43",
-      methodManagement: PaymentMethodManagementTypeEnum.ONBOARDABLE,
-      name: "PostePay",
-      paymentTypeCode: "PPAY",
+      asset: "https://assets.cdn.io.italia.it/logos/apps/paga-con-postepay.png",
+      description: "Paga con Poste Pay",
+      id: "1c12349f-8133-42f3-ad96-7e6527d27a41",
+      methodManagement: PaymentMethodManagementTypeEnum.REDIRECT,
+      name: "Paga con Poste Pay",
+      paymentTypeCode: "RBPP",
+      ranges: [
+        {
+          max: 999999 as NonNegativeInteger,
+          min: 0 as NonNegativeInteger
+        }
+      ],
+      status: PaymentMethodStatusEnum.ENABLED
+    },
+    {
+      description: "Paga con BancoPosta",
+      id: "1123429f-8133-42f3-ad96-7e6527d27a43",
+      methodManagement: PaymentMethodManagementTypeEnum.REDIRECT,
+      name: "Paga con BancoPosta",
+      paymentTypeCode: "RBPR",
+      ranges: [
+        {
+          max: 999999 as NonNegativeInteger,
+          min: 0 as NonNegativeInteger
+        }
+      ],
+      status: PaymentMethodStatusEnum.ENABLED
+    },
+    {
+      description: "Paga con BancoPosta per le imprese",
+      id: "1c12349f-8133-42f3-ad96-7e6527d27a40",
+      methodManagement: PaymentMethodManagementTypeEnum.REDIRECT,
+      name: "Paga con BancoPosta per le imprese",
+      paymentTypeCode: "RBPB",
+      ranges: [
+        {
+          max: 999999 as NonNegativeInteger,
+          min: 0 as NonNegativeInteger
+        }
+      ],
+      status: PaymentMethodStatusEnum.ENABLED
+    },
+    {
+      description: "Paga in conto Intesa",
+      id: "1c21234f-8133-42f3-ad96-7e6527d27a45",
+      methodManagement: PaymentMethodManagementTypeEnum.REDIRECT,
+      name: "Paga in conto Intesa",
+      paymentTypeCode: "RPIC",
+      ranges: [
+        {
+          max: 999999 as NonNegativeInteger,
+          min: 0 as NonNegativeInteger
+        }
+      ],
+      status: PaymentMethodStatusEnum.ENABLED
+    },
+    {
+      asset: "https://assets.cdn.io.italia.it/logos/apps/satispay.png",
+      description: "Paga con Satispay",
+      id: "0021234f-12345-42f3-ad96-7e6527d27a44",
+      methodManagement: PaymentMethodManagementTypeEnum.REDIRECT,
+      name: "Paga con Satispay",
+      paymentTypeCode: "SATY",
       ranges: [
         {
           max: 999999 as NonNegativeInteger,
