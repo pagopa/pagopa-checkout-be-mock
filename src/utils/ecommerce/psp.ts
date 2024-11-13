631d06ffe8433bb967d2a72e707e0c692b53a8b3
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2024-11-13 10:15:09 +0100

    update psp result list

--- src/utils/ecommerce/psp.ts
+++ src/utils/ecommerce/psp.ts
@@ -114,7 +114,7 @@ const createSuccessGetPspByPaymentMethodsIdResponseEntityBelowThresholdV2 = (): 
       onUs: true,
       paymentMethod: "CP",
       pspBusinessName: "Worldline Merchant Services Italia S.p.A.",
-      taxPayerFee: 95,
+      taxPayerFee: 15,
       touchpoint: "CHECKOUT"
     },
     {
@@ -158,31 +158,47 @@ const createSuccessGetPspByPaymentMethodsIdResponseEntityUpThresholdV2 = (): Cal
   brandAssets: cardBrandAssets,
   bundles: [
     {
-      abi: "AMREX",
-      bundleDescription: "bundle 3 OnUs",
-      bundleName: "BUNDLE3 OnUs - OLD",
-      idBrokerPsp: "123445",
-      idBundle: "789",
-      idChannel: "aasd",
-      idPsp: "PTG3",
+      abi: "33111",
+      bundleDescription: "Pagamenti con carte",
+      bundleName: "Worldline Merchant Services Italia S.p.A.",
+      idBrokerPsp: "05963231005",
+      idBundle: "98d24e9a-ab8b-48e3-ae84-f0c16c64db3b",
+      idChannel: "05963231005_01",
+      idPsp: "BNLIITRR",
       onUs: true,
-      pspBusinessName: "BUNDLE3 ONUS",
-      taxPayerFee: 20,
-      touchpoint: "FHGHF"
+      paymentMethod: "CP",
+      pspBusinessName: "Worldline Merchant Services Italia S.p.A.",
+      taxPayerFee: 15,
+      touchpoint: "CHECKOUT"
     },
     {
-      abi: "AMREX",
-      bundleDescription: "bundle 4",
-      bundleName: "BUNDLE4 - OLD",
-      idBrokerPsp: "3243",
-      idBundle: "012",
-      idChannel: "aasd",
-      idPsp: "PTG4",
+      abi: "WOLLNLB1",
+      bundleDescription: "Pacchetto test 500",
+      bundleName: "WP 100-500",
+      idBrokerPsp: "NL853935051B01",
+      idBundle: "189073b8-7b25-4296-a38d-774f286f3823",
+      idChannel: "NL853935051B01_02",
+      idPsp: "WOLLNLB1",
       onUs: false,
-      paymentMethod: "paymentMethod4",
-      pspBusinessName: "BUNDLE4",
-      taxPayerFee: 25,
-      touchpoint: "FHXHF"
+      paymentMethod: "CP",
+      pspBusinessName: "Worldpay BV",
+      taxPayerFee: 0,
+      touchpoint: "CHECKOUT"
+    },
+    {
+      abi: "03069",
+      bundleDescription:
+        "Clienti e non delle Banche del Gruppo Intesa Sanpaolo possono disporre pagamenti con carte di pagamento VISA-MASTERCARD",
+      bundleName: "Intesa Sanpaolo S.p.A",
+      idBrokerPsp: "00799960158",
+      idBundle: "fbde6612-aa57-4985-9557-8a1c9284add4",
+      idChannel: "00799960158_07",
+      idPsp: "BCITITMM",
+      onUs: false,
+      paymentMethod: "CP",
+      pspBusinessName: "Intesa Sanpaolo S.p.A",
+      taxPayerFee: 100,
+      touchpoint: "CHECKOUT"
     }
   ],
   paymentMethodDescription: "payment method description (v2)",
