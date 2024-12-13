08799d06aec754ed357e1f3c9a30eaabfe5d00f3
e.serrao <emanuele.serrao@bvtech.com>
2024-12-13 08:58:07 +0100

    finalized

--- src/utils/ecommerce/transaction.ts
+++ src/utils/ecommerce/transaction.ts
@@ -17,8 +17,6 @@ export const createSuccessGetTransactionEntity = (
   nodeInfo?: TransactionInfoNodeInfo,
   gatewayInfo?: TransactionInfoGatewayInfo
 ): TransactionInfo => ({
-  authToken:
-    "eyJhbGciOiJIUzUxMiJ9.eyJ0cmFuc2FjdGlvbklkIjoiMTdhYzhkZTMtMjAzMy00YzQ2LWI1MzQtZjE5MTk2NmNlODRjIiwicnB0SWQiOiI3Nzc3Nzc3Nzc3NzMzMDIwMDAwMDAwMDAwMDAwMCIsImVtYWlsIjoibmFtZS5zdXJuYW1lQHBhZ29wYS5pdCIsInBheW1lbnRUb2tlbiI6IjRkNTAwZTk5MDg3MTQyMDJiNTU3NTFlZDZiMWRmZGYzIiwianRpIjoiODUxNjQ2NDQzMjUxMTQxIn0.Fl3PoDBgtEhDSMFR3unkAow8JAe2ztYDoxlu7h-q_ygmmGvO7zP5dlztELUQCofcmYwhB4L9EgSLNT-HbiJgKA",
   clientId: ClientIdEnum.CHECKOUT,
   feeTotal: 99999999,
   gatewayInfo,
