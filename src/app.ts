15c89db296c7fb98ab5f1b14000cc9f6780539b9
scaminati-bv <simone.caminati@bvtech.com>
2025-02-20 15:10:08 +0100

    feat: add checkout feature flag

--- src/app.ts
+++ src/app.ts
@@ -38,6 +38,7 @@ import {
   retrieveCardDataFromNpg
 } from "./handlers/ecommerce/payment-method";
 import { checkoutAuthServiceLogin } from "./handlers/checkout-auth-service";
+import { checkoutFeatureFlag } from "./handlers/checkout-feature-flags";
 
 // eslint-disable-next-line max-lines-per-function
 export const newExpressApp: () => Promise<Express.Application> = async () => {
@@ -229,6 +230,9 @@ export const newExpressApp: () => Promise<Express.Application> = async () => {
 
   // checkout-auth-service login mock
   app.get("/checkout/auth-service/v1/auth/login", checkoutAuthServiceLogin);
+
+  // checkout feature flags mock
+  app.get("/checkout/feature-flags/v1", checkoutFeatureFlag);
 
   return app;
 };
