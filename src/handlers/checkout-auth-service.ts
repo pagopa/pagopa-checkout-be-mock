4101d923dd585bfcbf4f79242e04093dafd6a0fd
scaminati-bv <simone.caminati@bvtech.com>
2025-02-26 11:17:13 +0100

    fix: update auth/token api

--- src/handlers/checkout-auth-service.ts
+++ src/handlers/checkout-auth-service.ts
@@ -2,12 +2,14 @@ import { RequestHandler } from "express";
 import { LoginResponse } from "../generated/checkout-auth-service-v1/LoginResponse";
 import { logger } from "../logger";
 import { AuthResponse } from "../generated/checkout-auth-service-v1/AuthResponse";
+import { AuthRequest } from "../generated/checkout-auth-service-v1/AuthRequest";
+import { ProblemJson } from "../generated/checkout-auth-service-v1/ProblemJson";
 
 export const checkoutAuthServiceLogin: RequestHandler = async (_req, res) => {
   logger.info("[Get Auth Login] - Return success");
   const loginResponse: LoginResponse = {
     urlRedirect:
-      "http://localhost:1234/auth-callback?auth-code=J0NYD7UqPejqXpl6Fdv8"
+      "http://localhost:1234/auth-callback?code=J0NYD7UqPejqXpl6Fdv8&state=1BWuOGF4L3CTroTEvUVF"
   };
   res.status(200).send(loginResponse);
 };
@@ -16,9 +18,13 @@ export const checkoutAuthServicePostToken: RequestHandler = async (
   req,
   res
 ) => {
-  if (!req.body.authCode) {
-    logger.info("[Post Auth Token] - Error Missing authCode");
-    res.status(500).send();
+  const body: AuthRequest = req.body;
+  if (!body.authCode || !body.state) {
+    logger.info("[Post Auth Token] - Error Missing authCode or state");
+    const response: ProblemJson = {
+      title: "AuthCode or state is missing"
+    };
+    res.status(500).send(response);
     return;
   }
 
