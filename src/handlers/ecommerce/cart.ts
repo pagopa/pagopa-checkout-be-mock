5c775d35c5601d09b30abbb5ce6c0eae6547e63a
e.serrao <emanuele.serrao@bvtech.com>
2025-02-27 12:17:54 +0100

    fixed tsconfig.json

--- src/handlers/ecommerce/cart.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { RequestHandler } from "express";
-import { logger } from "../../logger";
-import { createSuccessGetCartResponseEntity } from "../../utils/ecommerce/cart";
-
-export const ecommerceGetCart: RequestHandler = async (req, res) => {
-  logger.info("[Get cart ecommerce] - Return success case");
-  res.status(200).send(createSuccessGetCartResponseEntity());
-};
