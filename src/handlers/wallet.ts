a619bdf1f2d13a615260c30d6e121e69ce000b61
Forciniti Luigi <luigi.forciniti.ext@jakala.com>
2025-12-10 10:09:32 +0100

    [CHK-4524] feat:  create mock endpoint for fetching wallets (#115)
    * feat:  create mock endpoint for fetching wallets
    
    * chore: set logo paypal in wallet list
    
    * feat: add new FeatureFlag  = isPaymentWalletEnabled
    
    * chore: modify end pint wallet
    
    * fix: set v1.727.0 for openapi wallet
    
    * chore: update infra ref
    
    ---------
    
    Co-authored-by: pietro.tota <pietro.tota@pagopa.it>

--- src/handlers/wallet.ts
+++ src/handlers/wallet.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, sort-keys */
 import { pipe } from "fp-ts/function";
 import * as E from "fp-ts/Either";
 import { fold, map } from "fp-ts/Either";
@@ -27,6 +28,12 @@ import { AddWalletUsingPOSTT } from "../generated/payment_manager/requestTypes";
 import { WalletResponse } from "../generated/payment_manager/WalletResponse";
 import { FlowCase, getFlowCookie } from "../flow";
 import { logger } from "../logger";
+import { WalletInfo } from "../generated/ecommerce-wallet/WalletInfo";
+import { WalletInfoDetails } from "../generated/ecommerce-wallet/WalletInfoDetails";
+import { WalletClient } from "../generated/ecommerce-wallet/WalletClient";
+import { WalletApplicationInfo } from "../generated/ecommerce-wallet/WalletApplicationInfo";
+import { WalletStatusEnum } from "../generated/ecommerce-wallet/WalletStatus";
+import { WalletClientStatusEnum } from "../generated/ecommerce-wallet/WalletClientStatus";
 
 // eslint-disable-next-line functional/no-let
 let creditCard: CreditCard;
@@ -145,4 +152,54 @@ export const updateWalletHandler: RequestHandler = async (req, res) => {
     tupleWith(res),
     fold(_e => res.status(500).send(), sendResponseWithData)
   );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, sort-keys
+const mockWallets: ReadonlyArray<WalletInfo> = [
+  {
+    walletId: "11111111-1111-1111-1111-111111111111",
+    paymentMethodId: "cf3cc414-3b6f-46f6-a0ae-0f2e96188a56",
+    paymentMethodAsset:
+      "https://assets.cdn.platform.pagopa.it/creditcard/generic.png",
+    status: WalletStatusEnum.VALIDATED,
+    creationDate: new Date(),
+    updateDate: new Date(),
+    applications: [{ name: "APP1", status: "ENABLED" }] as ReadonlyArray<
+      WalletApplicationInfo
+    >,
+    clients: {
+      IO: { status: WalletClientStatusEnum.ENABLED }
+    },
+    details: {
+      type: "CARDS",
+      brand: "VISA",
+      lastFourDigits: "1334",
+      expiryDate: "203012"
+    } as WalletInfoDetails
+  },
+  {
+    walletId: "22222222-2222-2222-2222-222222222222",
+    paymentMethodId: "8f2a657e-4dd1-4f1a-9c48-c9df81203699",
+    paymentMethodAsset: "https://assets.cdn.platform.pagopa.it/apm/paypal.png",
+    status: WalletStatusEnum.VALIDATED,
+    creationDate: new Date(),
+    updateDate: new Date(),
+    applications: [{ name: "APP2", status: "DISABLED" }] as ReadonlyArray<
+      WalletApplicationInfo
+    >,
+    clients: {
+      IO: { status: WalletClientStatusEnum.DISABLED }
+    },
+    details: {
+      type: "PAYPAL",
+      pspId: "psp_123",
+      pspBusinessName: "PayPal Business",
+      maskedEmail: "test***@***test.it"
+    } as WalletInfoDetails
+  }
+];
+
+export const ecommerceGetWallets: RequestHandler = async (_req, res) => {
+  logger.info("GET WALLLETS ");
+  res.json({ wallets: mockWallets });
 };
